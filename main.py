6f1e624cc15d8582dc40b3805ba73e98c2f4675c
gareev_mr <maratgareev97@gmail.com>
2022-09-29 16:43:33 +0300

    get

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, request
+import requests
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/test')
 def vaso():
     a=6
     b=7
@@ -10,8 +11,27 @@ def vaso():
     print(a+b)
     return "Привет <b>мир</b>"+str(s)+str(a)
 
-@app.route('/test')
-def vaso1():
-    return "Тест"
+@app.route('/variable/<vvv>---<mmm>')
+def index1(vvv,mmm):
+    mas=[]
+    mas.append(vvv)
+    mas.append(mmm)
+    print(int(mas[0])+int(mas[1]))
+    string=str(mas)
+    return string+"   "+ str(int(mas[0])+int(mas[1]))
 
+@app.route('/get/', methods=['GET'])
+def index2():
+    a=request.args.get('a')
+    b=request.args.get('b')
+    return a+" "+b
+
+@app.route('/')
+def index():
+    a="Hello "
+    b="world"
+    res = requests.get("http://127.0.0.1:5000/get/?a={}&b={}".format(a,b))
+    print(res.text)
+    print(res)
+    return "Привет" + res.text
 app.run()
